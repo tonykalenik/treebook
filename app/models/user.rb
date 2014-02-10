5b493569f79a7a04a819150b46dcb3ea5e2aedf3
Anton Kalenik <anton@kalenik.name>
2014-02-10 20:42:03 +0300

    закончили тесты на введение имени, фамилии и ника

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true
+
   has_many :statuses
 
   def full_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,17 @@ class UserTest < ActiveSupport::TestCase
     assert !user.save
     assert !user.errors[:first_name].empty?
   end
+
+  test 'a user should enter a last name' do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test 'a user should enter a profile name' do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
 end
