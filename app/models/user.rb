f320ca1875e5f819db8dda4e61ddc1e1dc098e1e
Anton Kalenik <anton@kalenik.name>
2014-02-10 20:33:43 +0300

    улучшили проверку

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, presence: true
   has_many :statuses
 
   def full_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,5 +4,6 @@ class UserTest < ActiveSupport::TestCase
   test 'a user should enter a first name' do
     user = User.new
     assert !user.save
+    assert !user.errors[:first_name].empty?
   end
 end
