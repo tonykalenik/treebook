36ae623354ebfa3cfdfecdfbb4477f4bcafcf3b8
Anton Kalenik <anton@kalenik.name>
2014-02-12 20:51:09 +0300

    тест для логин страницы

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CustomRoutesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'that /login route opens the login page' do
+    get '/login'
+    assert_response :success
+  end
+
+  test 'that /logout route opens the log out page' do
+    get '/logout'
+    assert_response :success
+  end
 end
