638cf8ea9af73e50f9362a9aa6dab4481fc0d459
Anton Kalenik <anton@kalenik.name>
2014-02-13 20:48:15 +0300

    тест на не пустой пост

--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'that a status requires content' do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
 end
