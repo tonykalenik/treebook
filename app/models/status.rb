804afcb9a06d4e03058539d4d6f5c1ab3f7857c6
Anton Kalenik <anton@kalenik.name>
2014-02-13 21:07:47 +0300

    у поста обязательно должен быть user_id

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,6 @@ class Status < ActiveRecord::Base
   belongs_to :user
 
   validates :content, presence: true, length: {minimum: 2}
+
+  validates :user_id, presence: true
 end
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -13,4 +13,11 @@ class StatusTest < ActiveSupport::TestCase
     assert !status.save
     assert !status.errors[:content].empty?
   end
+
+  test 'that a status has a user id' do
+    status = Status.new
+    status.content = 'Hello!'
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+  end
 end
